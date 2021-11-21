330b8e2785e87c7a469c5e4c5351ed6fc2ba3539
GiovannaRMendes <giovanna223377@gmail.com>
2021-11-20 23:48:01 -0400

    Adicionando exercício

---
+++ C/1242.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#define max 300
+
+void empilha(int *t, char c, char pilha[]) {
+    (*t)++;
+    pilha[*t] = c;
+}
+
+int main(void) {
+    char ar[max], l;
+    int qtd = 0, t = -1;
+
+    while (scanf("%c", &l) != EOF) {
+
+        if (l == '\n') {
+            printf("%d\n", qtd);
+            qtd = 0;
+            t = -1;
+        } else if ((l == 'S' && ar[t] == 'B') || (l == 'B' && ar[t] == 'S')) {
+            t--;
+            qtd++;
+        } else if ((l == 'F' && ar[t] == 'C') || (l == 'C' && ar[t] == 'F')) {
+            t--;
+            qtd++;
+        } else {
+            empilha(&t, l, ar);
+        }
+
+    }
+    return 0;
+}
